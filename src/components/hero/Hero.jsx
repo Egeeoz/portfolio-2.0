c5e0ef296c5a3a9e181793c8ffd79e71947bcc62
Egeeoz <ozakdemirege@hotmail.com>
2025-02-27 15:10:38 +0100

    Implement scroll animation for hero content

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 import styles from "./Hero.module.css";
+import { FaChevronDown } from "react-icons/fa6";
 
 const Hero = () => {
+  const heroContentRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (heroContentRef.current) {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 50) {
+          heroContentRef.current.style.opacity = Math.max(
+            1 - scrollPosition / 300,
+            0
+          ).toString();
+          heroContentRef.current.style.transform = `translateY(${
+            scrollPosition * -0.2
+          }px)`;
+        } else {
+          heroContentRef.current.style.opacity = "1";
+          heroContentRef.current.style.transform = "translateY(0)";
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section className={styles.heroContainer}>
-      <h1>ege özakdemir</h1>
-      <h2>javascript developer</h2>
+      <div className={styles.heroContent} ref={heroContentRef}>
+        <h1>ege özakdemir</h1>
+        <h2>javascript developer</h2>
+      </div>
+      <FaChevronDown size={20} />
     </section>
   );
 };
