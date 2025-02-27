d6515ca7c9a77573b81caf16a9b073b1dd365d37
Egeeoz <ozakdemirege@hotmail.com>
2025-02-27 14:06:16 +0100

    Update navbar positioning, update variable colors

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import styles from './Navbar.module.css';
-import { FiLinkedin, FiGithub } from 'react-icons/fi';
+import { useEffect, useState } from "react";
+import styles from "./Navbar.module.css";
+import { FiLinkedin, FiGithub } from "react-icons/fi";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -9,18 +9,20 @@ const Navbar = () => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <nav className={isScrolled ? styles.scrolled : ''}>
-      <h1>EÖ</h1>
-      <ul>
+    <nav className={isScrolled ? styles.scrolled : ""}>
+      <ul className={styles.leftNavLinks}>
+        <li>Projects</li>
         <li>About</li>
         <li>Skills</li>
-        <li>Projects</li>
         <li>Contact</li>
+      </ul>
+      <h1>Ege Özakdemir</h1>
+      <ul className={styles.rightNavLinks}>
         <li>
           <FiLinkedin size={20} />
         </li>
