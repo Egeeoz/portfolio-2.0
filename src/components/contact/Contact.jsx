8ccd3be053da32d65a406c92af2167cd2040e1df
Egeeoz <ozakdemirege@hotmail.com>
2025-03-20 13:28:24 +0100

    Create contact section with styling, fix skills section styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import styles from './Contact.module.css';
+import { FiLinkedin, FiGithub, FiPhone } from 'react-icons/fi';
+import { IoMailOutline } from 'react-icons/io5';
+import { SlLocationPin } from 'react-icons/sl';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className={styles.contactContainer}>
+      <h1 className={styles.contactTitle}>Contact</h1>
+      <div className={styles.contactInfo}>
+        <p>
+          <IoMailOutline />
+          ozakdemirege@hotmail.com
+        </p>
+        <p>
+          <FiPhone />
+          072 018 25 03
+        </p>
+        <p>
+          <SlLocationPin />
+          Sweden, Stockholm
+        </p>
+      </div>
+      <div className={styles.contactLinks}>
+        <a href="">
+          <FiGithub size={25} />
+        </a>
+        <a href="">
+          <FiLinkedin size={25} />
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
